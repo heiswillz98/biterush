d7d3e74126a1339129dc5d379c41ee7a58af3636
heiswillz98 <wheeliams98@gmail.com>
2024-06-16 19:53:51 +0100

    fix: refactor

--- src/components/Hero/Hero2.tsx
+++ src/components/Hero/Hero2.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode } from "react";
 import { MdPayment } from "react-icons/md";
 import { TbTruckDelivery } from "react-icons/tb";
 import { VscGoToSearch } from "react-icons/vsc";
+import { H11, H4, P } from "../Shared/heading/Heading";
 
 interface Hero2CardProps {
   icon: ReactNode;
@@ -13,13 +14,12 @@ const Hero2Card: React.FC<Hero2CardProps> = ({ icon, title, description }) => {
   return (
     <div className="flex flex-col gap-2 items-center justify-center">
       <div className="text-6xl text-[#41c09e]">{icon}</div>
-
-      <h1 className="text-[#000000] text-lg font-medium lg:text-2xl">
-        {title}
-      </h1>
-      <p className="text-center text-base lg:text-lg text-[#666666] w-11/12 md:w-full">
-        {description}
-      </p>
+      <div className=" flex flex-col gap-2">
+        <H4 className=" font-medium text-center">{title}</H4>
+        <P className="text-center text-base lg:text-lg text-[#666666] w-11/12 md:w-full">
+          {description}
+        </P>
+      </div>
     </div>
   );
 };
@@ -28,10 +28,10 @@ const Hero2 = () => {
   return (
     <section className="my-6 md:my-10">
       <div className="flex flex-col justify-center items-center">
-        <h1 className="text-2xl font-bold my-4 text-center md:text-3xl md:ml-2">
+        <H11 className="text-2xl  my-4 text-center md:text-3xl md:ml-2">
           How it Works
-        </h1>
-        <div className="flex flex-col items-center justify-center gap-4 md:gap-1 md:flex-row md:mx-3 md:mr-8 lg:mx-20 lg:mr-40">
+        </H11>
+        <div className="flex flex-col items-center justify-center gap-4 lg:gap-1 md:flex-row md:mx-3 md:mr-8 lg:mx-20 lg:mr-40">
           <Hero2Card
             icon={<VscGoToSearch />}
             title={"1. Search"}
@@ -48,7 +48,7 @@ const Hero2 = () => {
           />
           <Hero2Card
             icon={<TbTruckDelivery />}
-            title={"3. Delivery or Pick-up"}
+            title={"3. Delivery"}
             description={
               " Have your meal delivered Hot & Fast or Self-pick. Enjoy your meal!"
             }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,10 @@
 import React, { useState } from "react";
 import styles from "./Navbar.module.scss";
 import Link from "next/link";
-import { MdOutlinePerson } from "react-icons/md";
 import { BsBag } from "react-icons/bs";
-import { BiSearch } from "react-icons/bi";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { LiaTimesSolid } from "react-icons/lia";
 import { SiCodechef } from "react-icons/si";
-// import { Button } from "react-bootstrap";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false); // State to track menu visibility
@@ -27,14 +24,14 @@ const Navbar = () => {
   ];
 
   return (
-    <div className="fixed top-0 w-full bg-white z-50">
-      <div className="bg-[#41c09e] h-8 md:h-12">
-        <p className="font-[900] text-white text-xs md:text-sm flex items-center justify-center pt-2">
+    <div className={styles.Container}>
+      <div className={styles.ContainerH}>
+        <p className={styles.ContainerP}>
           Need help with your order? Please Call 0700 4000 100 {">>>"}
         </p>
       </div>
-      <div className="flex h-[2rem] gap-8 items-center justify-center mx-8 mt-8">
-        <div className="flex mb-6 items-center">
+      <div className={styles.Header}>
+        <div className={styles.HeaderH}>
           <div className="lg:hidden cursor-pointer" onClick={toggleMenu}>
             {/* Hamburger icon */}
             <div
@@ -45,32 +42,29 @@ const Navbar = () => {
               <GiHamburgerMenu />
             </div>
             {isMenuOpen && (
-              <div
-                className="left-[3rem] text-[22px] bottom-[44rem] cursor-pointer "
-                onClick={closeMenu}
-              >
+              <div className={styles.Lia} onClick={closeMenu}>
                 <LiaTimesSolid />
               </div>
             )}
           </div>
-          <div className="flex gap-6 mx-[2rem] items-center lg:w-[1340px]">
-            <div className="flex flex-row items-center text-lg md:text-xl gap-2">
+          <div className={styles.Bottom}>
+            <div className={styles.BottomP}>
               <h1 className=" whitespace-nowrap">BITE RUSH</h1>
-              <div className="bg-[white] flex items-center h-9">
-                <div className="color-[#fff] text-2xl">
+              <div className={styles.Si}>
+                <div className={styles.SiC}>
                   <SiCodechef style={{ color: "#41c09e" }} />
                 </div>
               </div>
             </div>
 
-            <div className="hidden mx-[23rem] lg:flex flex-row gap-8">
+            <div className={styles.Navs}>
               {navItems.map((item, index) => (
                 <Link href={item.link} key={index}>
                   <h1 className={styles.nav_links}>{item.text}</h1>
                 </Link>
               ))}
             </div>
-            <div className="flex flex-row gap-4 text-[20px]  ml-auto">
+            <div className={styles.BsBag}>
               <BsBag />
             </div>
           </div>
@@ -79,7 +73,7 @@ const Navbar = () => {
 
       {/* MOBILE SCREEN */}
       {isMenuOpen && (
-        <div className="lg:hidden absolute bg-[white] flex flex-col gap-6 h-[100rem] w-[400px] pt-[2rem] pl-6  border-r-2 z-50">
+        <div className={styles.MobileNav}>
           {navItems.map((item, index) => (
             <div key={index}>
               <Link href={item.link}>
